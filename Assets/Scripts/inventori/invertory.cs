aedde1d99c776714f6d2e1357e597ef5143289f8
Dum4ick <duduk.maksim@gmail.com>
2024-12-09 00:51:51 +0300

    Slots anim

--- Assets/Scripts/inventori/invertory.cs
+++ Assets/Scripts/inventori/invertory.cs
@@ -5,34 +5,67 @@ using UnityEngine;
 public class Invertory : MonoBehaviour
 {
     public bool[] isFull;
+
     public GameObject[] slots;
+
     public int numSlot = 0;
 
+    private GameObject slot;
+
+    Animator anim;
+    private void Start()
+    {
+        PlayAnims(0);
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            numSlot = 0;
+
+            PlayAnims(0);
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            numSlot = 1;
+
+            PlayAnims(1);
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            numSlot = 2; 
+
+            PlayAnims(2);
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            numSlot = 3;
+
+            PlayAnims(3);
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            numSlot = 4;
+
+            PlayAnims(4);
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha6))
         {
-            numSlot = 5;
+
+            PlayAnims(5);
+
         }
+    }
+
+    public void PlayAnims(int newNumSlot)
+    {
+        slot = GameObject.Find($"Slot ({numSlot + 1})");
+        anim = slot.GetComponent<Animator>();
+        anim.Play("Disabled");
+        numSlot = newNumSlot;
+
+        slot = GameObject.Find($"Slot ({numSlot + 1})");
+        anim = slot.GetComponent<Animator>();
+        anim.Play("Selected");
     }
 }   
